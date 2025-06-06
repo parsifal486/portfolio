1c68318dee3d21c048e9a779c4c6d9df260e303f
liuzeyu <mrliuzeyou@outlook.com>
2025-06-06 15:14:34 +0800

    Update dependencies and enhance styling; added new fonts and color variables. Removed unused SVG files and updated package configurations.

---
+++ app/componets/Navbar.tsx
@@ -0,0 +1,103 @@
+import React, { useRef } from 'react';
+import { logo, icon } from '@/assets/assets';
+import Image from 'next/image';
+
+export const Navbar = () => {
+  const mobileMenuRef = useRef<HTMLUListElement>(null);
+
+  const openMenu = () => {
+    if (mobileMenuRef.current) {
+      mobileMenuRef.current.style.transform = 'translateX(-16rem)';
+    }
+  };
+
+  const closeMenu = () => {
+    if (mobileMenuRef.current) {
+      mobileMenuRef.current.style.transform = 'translateX(16rem)';
+    }
+  };
+
+  return (
+    <nav className="bg-purplespace-200 fixed z-50 flex h-16 w-full flex-row items-center justify-between px-5 py-4 lg:px-8 xl:px-[8%]">
+      <a className="flex flex-row items-center gap-2">
+        <Image src={logo.dimond1} alt="logo" width={50} height={50} />
+        <div className="text-2xl font-bold">Ryuteakwoo's</div>
+      </a>
+
+      <ul className="font-outfit hidden flex-row items-center justify-between gap-8 rounded-full px-12 py-3 md:flex lg:gap-10">
+        <li>
+          <a href="#top">Home</a>
+        </li>
+        <li>
+          <a href="#product">Product</a>
+        </li>
+        <li>
+          <a href="#post">Post</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="flex flex-row items-center gap-4">
+        <button>
+          <icon.daymode size={25} />
+        </button>
+
+        <a
+          href="#contact"
+          className="ml-4 hidden items-center gap-1 rounded-full border border-gray-500 px-3 py-2.5 pl-5 font-bold lg:flex"
+        >
+          Contact Now
+          <icon.contact size={20} />
+        </a>
+
+        <button className="ml-3 block lg:hidden" onClick={openMenu}>
+          <icon.menu size={25} />
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      <ul
+        ref={mobileMenuRef}
+        className="bg-purplespace-200 fixed top-0 -right-64 bottom-0 flex h-screen w-64 flex-col gap-4 px-10 py-20 transition duration-300 md:hidden"
+      >
+        <button
+          className="absolute top-5 right-5 cursor-pointer"
+          onClick={closeMenu}
+        >
+          <icon.closepanel size={25} />
+        </button>
+
+        <li>
+          <a href="#top" onClick={closeMenu}>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#product" onClick={closeMenu}>
+            Product
+          </a>
+        </li>
+        <li>
+          <a href="#post" onClick={closeMenu}>
+            Post
+          </a>
+        </li>
+        <li>
+          <a href="#about" onClick={closeMenu}>
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#contact" onClick={closeMenu}>
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
---
+++ assets/assets.js
@@ -0,0 +1,29 @@
+import dimond from './dimond.svg';
+import dimond1 from './dimond1.svg';
+import dimond2 from './dimond2.svg';
+import { BiChevronRight as contact } from 'react-icons/bi';
+import { BiMoon as daymode } from 'react-icons/bi';
+import { BiSolidMoon as nightmode } from "react-icons/bi";
+import { BiMenu as menu } from "react-icons/bi";
+import { BiArrowFromLeft as closepanel } from "react-icons/bi";
+import { Avatar } from './imgs/avatar.jpg';
+
+const logo = {
+  dimond,
+  dimond1,
+  dimond2,
+};
+
+const icon = {
+  contact,
+  daymode,
+  nightmode,
+  menu,
+  closepanel,
+};
+
+const img = {
+  avatar,
+};
+
+export { logo, icon, img };
