4fc14faf551b9d272e86d4c06ba1a370017acdc3
liuzeyu <mrliuzeyou@outlook.com>
2025-06-26 13:02:37 +0800

    Update import paths in page.tsx to use absolute paths for improved module resolution and consistency across the project.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,8 @@
-import { Navbar } from './components/Navbar';
-import { HomeSection } from './components/HomeSection';
-import { Works } from './components/Works';
-import { Posts } from './components/Posts';
-import { About } from './components/About';
+import { Navbar } from '@/components/Navbar';
+import { HomeSection } from '@/components/HomeSection';
+import { Works } from '@/components/Works';
+import { Posts } from '@/components/Posts';
+import { About } from '@/components/About';
 
 export default function Home() {
     return (
