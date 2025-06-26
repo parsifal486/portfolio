7e2e758329f00ca0e564c21fed635b5ce2520a0d
liuzeyu <mrliuzeyou@outlook.com>
2025-06-26 13:49:05 +0800

    Add webpack alias configuration in next.config.ts for improved module resolution

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
+import path from "path";
 
 const nextConfig: NextConfig = {
-  
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+      '@/public': path.resolve(__dirname, 'public'),
+      '@/content': path.resolve(__dirname, 'content'),
+      '@/messages': path.resolve(__dirname, 'messages'),
+      '@/i18n': path.resolve(__dirname, 'src/i18n'),
+      '@/components': path.resolve(__dirname, 'src/app/[locale]/components'),
+    };
+    return config;
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
